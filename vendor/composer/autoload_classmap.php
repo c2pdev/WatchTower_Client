7926e3607241ea4ac00cc0968c56e547fbfd27c9
Code2prog <dawid@code2prog.com>
2016-01-29 22:09:38 +0000

    fix name convention

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -405,8 +405,6 @@ return array(
     'PHP_Token_XOR_EQUAL' => $vendorDir . '/phpunit/php-token-stream/src/Token.php',
     'PHP_Token_YIELD' => $vendorDir . '/phpunit/php-token-stream/src/Token.php',
     'PHP_Token_YIELD_FROM' => $vendorDir . '/phpunit/php-token-stream/src/Token.php',
-    'PluginEndpoints\\Endpoint' => $vendorDir . '/dannyvankooten/plugin-endpoints/src/Endpoint.php',
-    'PluginEndpoints\\Router' => $vendorDir . '/dannyvankooten/plugin-endpoints/src/Router.php',
     'SebastianBergmann\\Comparator\\ArrayComparator' => $vendorDir . '/sebastian/comparator/src/ArrayComparator.php',
     'SebastianBergmann\\Comparator\\Comparator' => $vendorDir . '/sebastian/comparator/src/Comparator.php',
     'SebastianBergmann\\Comparator\\ComparisonFailure' => $vendorDir . '/sebastian/comparator/src/ComparisonFailure.php',
