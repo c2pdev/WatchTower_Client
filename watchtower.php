d104135612ae70a28b3c5313c8aaad1bd1b1d9ce
Code2prog <dawid@code2prog.com>
2016-01-31 10:09:42 +0000

    Cleanup composer dependencies

--- watchtower.php
+++ watchtower.php
@@ -4,7 +4,7 @@ Plugin Name: Whatarmy Watchtower
 Plugin URI:
 Description: WP website monitoring API
 Author: Code2prog
-Version: 1.0.9
+Version: 1.0.11
 Author URI:
 */
 require 'vendor/autoload.php';
