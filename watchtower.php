4d09b8173bc7e195c9464214fd74b55e733b2c59
Code2prog <dawid@code2prog.com>
2016-01-31 10:22:48 +0000

    Change Version

--- watchtower.php
+++ watchtower.php
@@ -4,7 +4,7 @@ Plugin Name: Whatarmy Watchtower
 Plugin URI: https://github.com/c2pdev/WatchTower_Client
 Description: WP website monitoring API
 Author: Code2prog
-Version: 1.0.13
+Version: 1.0.14
 Author URI: http://whatarmy.com
 */
 require 'vendor/autoload.php';
