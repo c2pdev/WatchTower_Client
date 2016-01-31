c4d259f74de92e65f67ff926cc58c4dc24792ee2
Code2prog <dawid@code2prog.com>
2016-01-31 11:26:01 +0000

    add missing phpdoc comments

--- watchtower.php
+++ watchtower.php
@@ -4,22 +4,34 @@ Plugin Name: Whatarmy Watchtower
 Plugin URI: https://github.com/c2pdev/WatchTower_Client
 Description: WP website monitoring API
 Author: Code2prog
-Version: 1.1.0
+Version: 1.1.1
 Author URI: http://whatarmy.com
 */
 
+/**
+ * Composer dependencies autoload
+ */
 require 'vendor/autoload.php';
+
 /**
  * UPDATE Plugin
  */
 require 'plugin-update-checker/plugin-update-checker.php';
-$className = PucFactory::getLatestClassVersion('PucGitHubChecker');
+$className       = PucFactory::getLatestClassVersion( 'PucGitHubChecker' );
 $myUpdateChecker = new $className(
 	'https://github.com/c2pdev/WatchTower_Client',
 	__FILE__,
 	'master'
 );
+
+/**
+ * Register autoload SRC
+ */
 register_wp_autoload( 'Whatarmy_Watchtower\\', __DIR__ . '/src' );
+
+/**
+ * Include Plugin Class
+ */
 if ( ! function_exists( 'get_plugins' ) ) {
 	require_once ABSPATH . 'wp-admin/includes/plugin.php';
 }
@@ -32,4 +44,8 @@ register_activation_hook( __FILE__, array( '\Whatarmy_Watchtower\Install_Uninsta
  * RUN API ENDPOINT
  */
 new \Whatarmy_Watchtower\Watchtower_API_Endpoint();
+
+/**
+ * Run Watchtower main class
+ */
 new \Whatarmy_Watchtower\Watchtower();
