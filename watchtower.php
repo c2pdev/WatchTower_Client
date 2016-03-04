4d3536e959fc909e143beaca542c1ba81387bcf5
Code2prog <dawid@code2prog.com>
2016-03-04 08:03:09 +0000

    wordpress installation size

--- watchtower.php
+++ watchtower.php
@@ -4,7 +4,7 @@ Plugin Name: Whatarmy Watchtower
 Plugin URI: https://github.com/c2pdev/WatchTower_Client
 Description: WP website monitoring API
 Author: Code2prog
-Version: 1.1.8
+Version: 1.1.9
 Author URI: http://whatarmy.com
 */
 
@@ -23,6 +23,19 @@ $myUpdateChecker = new $className(
 	__FILE__,
 	'master'
 );
+
+function auto_update_specific_plugins ( $update, $item ) {
+	// Array of plugin slugs to always auto-update
+	$plugins = array (
+		'watchtower',
+	);
+	if ( in_array( $item->slug, $plugins ) ) {
+		return true; // Always update plugins in this array
+	} else {
+		return $update; // Else, use the normal API response to decide whether to update or not
+	}
+}
+add_filter( 'auto_update_plugin', 'auto_update_specific_plugins', 10, 2 );
 
 /**
  * Register autoload SRC
