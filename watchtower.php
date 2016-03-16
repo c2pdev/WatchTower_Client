a1a5538f2c05f78571a47aaa27102a1d83893cc6
Code2prog <dawid@code2prog.com>
2016-03-16 04:04:38 +0000

    unzip file change to pecl library

--- watchtower.php
+++ watchtower.php
@@ -4,7 +4,7 @@ Plugin Name: Whatarmy Watchtower
 Plugin URI: https://github.com/c2pdev/WatchTower_Client
 Description: WP website monitoring API
 Author: Code2prog
-Version: 1.1.37
+Version: 1.1.38
 Author URI: http://whatarmy.com
 */
 
