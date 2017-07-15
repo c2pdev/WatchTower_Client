797b4a0774a38eca6629af0acd3e176520370b0a
Code2Prod <office@code2prog.com>
2017-07-16 00:57:49 +0100

    mysql backups

--- watchtower.php
+++ watchtower.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/c2pdev/WatchTower_Client
  * Description: The WhatArmy WordPress plugin allows us to monitor, backup, upgrade, and manage your site!
  * Author: Whatarmy
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author URI: http://whatarmy.com
  **/
 
