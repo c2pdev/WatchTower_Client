7fa6607b838c94bb88d615f026b16c648e490346
Niestabilny <dawid@code2prog.com>
2019-05-23 19:36:48 +0700

    1.7.1 - updates core.

--- libraries/action-schedule/classes/ActionScheduler_QueueRunner.php
+++ libraries/action-schedule/classes/ActionScheduler_QueueRunner.php
@@ -24,6 +24,17 @@ class ActionScheduler_QueueRunner extends ActionScheduler_Abstract_QueueRunner {
 	}
 
 	/**
+	 * ActionScheduler_QueueRunner constructor.
+	 *
+	 * @param ActionScheduler_Store             $store
+	 * @param ActionScheduler_FatalErrorMonitor $monitor
+	 * @param ActionScheduler_QueueCleaner      $cleaner
+	 */
+	public function __construct( ActionScheduler_Store $store = null, ActionScheduler_FatalErrorMonitor $monitor = null, ActionScheduler_QueueCleaner $cleaner = null ) {
+		parent::__construct( $store, $monitor, $cleaner );
+	}
+
+	/**
 	 * @codeCoverageIgnore
 	 */
 	public function init() {
@@ -39,24 +50,28 @@ class ActionScheduler_QueueRunner extends ActionScheduler_Abstract_QueueRunner {
 	}
 
 	public function run() {
-		@ini_set( 'memory_limit', apply_filters( 'admin_memory_limit', WP_MAX_MEMORY_LIMIT ) );
-		@set_time_limit( apply_filters( 'action_scheduler_queue_runner_time_limit', 600 ) );
+		ActionScheduler_Compatibility::raise_memory_limit();
+		ActionScheduler_Compatibility::raise_time_limit( $this->get_time_limit() );
 		do_action( 'action_scheduler_before_process_queue' );
 		$this->run_cleanup();
-		$count = 0;
-		if ( $this->store->get_claim_count() < apply_filters( 'action_scheduler_queue_runner_concurrent_batches', 5 ) ) {
+		$processed_actions = 0;
+		if ( $this->store->get_claim_count() < $this->get_allowed_concurrent_batches() ) {
 			$batch_size = apply_filters( 'action_scheduler_queue_runner_batch_size', 25 );
-			$count = $this->do_batch( $batch_size );
+			do {
+				$processed_actions_in_batch = $this->do_batch( $batch_size );
+				$processed_actions         += $processed_actions_in_batch;
+			} while ( $processed_actions_in_batch > 0 && ! $this->batch_limits_exceeded( $processed_actions ) ); // keep going until we run out of actions, time, or memory
 		}
 
 		do_action( 'action_scheduler_after_process_queue' );
-		return $count;
+		return $processed_actions;
 	}
 
 	protected function do_batch( $size = 100 ) {
 		$claim = $this->store->stake_claim($size);
 		$this->monitor->attach($claim);
 		$processed_actions = 0;
+
 		foreach ( $claim->get_actions() as $action_id ) {
 			// bail if we lost the claim
 			if ( ! in_array( $action_id, $this->store->find_actions_by_claim_id( $claim->get_id() ) ) ) {
@@ -64,6 +79,10 @@ class ActionScheduler_QueueRunner extends ActionScheduler_Abstract_QueueRunner {
 			}
 			$this->process_action( $action_id );
 			$processed_actions++;
+
+			if ( $this->batch_limits_exceeded( $processed_actions ) ) {
+				break;
+			}
 		}
 		$this->store->release_claim($claim);
 		$this->monitor->detach();
@@ -78,8 +97,6 @@ class ActionScheduler_QueueRunner extends ActionScheduler_Abstract_QueueRunner {
 	 * as well, so this is disabled by default. To enable:
 	 *
 	 * add_filter( 'action_scheduler_queue_runner_flush_cache', '__return_true' );
-	 *
-	 * @return void
 	 */
 	protected function clear_caches() {
 		if ( ! wp_using_ext_object_cache() || apply_filters( 'action_scheduler_queue_runner_flush_cache', false ) ) {
